d2396f66c9729626b3ba9a393c1cac72d7ac7b6c
Stuart Squibb <stuart_squibb@hotmail.co.uk>
2019-07-30 17:34:08 +0100

    Tidy code

--- OneNoteUtilitiesGraph/OneNoteUtilitiesGraph.psm1
+++ OneNoteUtilitiesGraph/OneNoteUtilitiesGraph.psm1
@@ -6,7 +6,7 @@ $ScriptRoot = Split-Path $Script:MyInvocation.MyCommand.Path
 $Public  = @( Get-ChildItem -Path $ScriptRoot\Public\*.ps1 -ErrorAction SilentlyContinue )
 $Private = @( Get-ChildItem -Path $ScriptRoot\Private\*.ps1 -ErrorAction SilentlyContinue )
 @($Public + $Private)  | Foreach-Object { . $_.FullName }
-Export-ModuleMember -Function $Public.Basename -Verbose
+Export-ModuleMember -Function $Public.Basename
 
 # Get settings from config file
 Get-ONConfig
